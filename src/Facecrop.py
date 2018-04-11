c08a1a5f886c61932d6adad7a22c90ee9f142221
Vahe303ub <vahe527887@yandex.ru>
2018-04-11 18:30:23 +0300

    moddified notebook and changed  Facecrop (deleting bad imports)

--- src/Facecrop.py
+++ src/Facecrop.py
@@ -1,6 +1,4 @@
 import face_recognition
-import scipy as sp
-import scipy.misc
 import matplotlib.pyplot as plt
 import numpy as np
 from imageio import imread
@@ -8,8 +6,8 @@ from imageio import imread
 def get_face(img=None,url=None):
     face_locations=None
     if img is None:
-        image = face_recognition.load_image_file("Jessica-Alba-Hot-photoshoot-2-834x1024.jpg")
-        face_locations = face_recognition.face_locations(image)
+        img = face_recognition.load_image_file(url)
+        face_locations = face_recognition.face_locations(img)
     else:
         face_locations = face_recognition.face_locations(img)
     img =img[list(face_locations[0])[0]:list(face_locations[0])[2],list(face_locations[0])[3]:list(face_locations[0])[1]]
